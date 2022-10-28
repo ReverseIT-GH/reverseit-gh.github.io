baf17edbad81adf623513b1aba2c29757a497d6d
Luca D'Amico <damico.luca91@live.it>
2022-10-28 19:10:09 +0200

    Fixed library widget layout

--- src/lib/library-widget/library-widget.svelte
+++ src/lib/library-widget/library-widget.svelte
@@ -4,10 +4,12 @@
     let libraryItems: LibraryItem[] = libraryIndex.slice(-10).reverse();
 </script>
 
-{#each libraryItems as libraryItem}
-<li class="my-1">
-	<div class="tooltip inline" data-tip="{new Date(Date.parse(libraryItem.publication_date)).toLocaleDateString()}">
-		<h3 class="inline"><a class="link" href="{libraryItem.download_url}" target="_blank">{@html libraryItem.title}</a></h3>
-	</div>
-</li>
-{/each}
+<ul class="list-disc list-outside text-left px-8">
+    {#each libraryItems as libraryItem}
+    <li class="my-1">
+        <div class="tooltip inline" data-tip="{new Date(Date.parse(libraryItem.publication_date)).toLocaleDateString()}">
+            <h3 class="inline"><a class="link" href="{libraryItem.download_url}" target="_blank">{@html libraryItem.title}</a></h3>
+        </div>
+    </li>
+    {/each}
+</ul>
