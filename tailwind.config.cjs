51b06c24ab9745388ac52c7b2d8a271d2e124922
Luca D'Amico <damico.luca91@live.it>
2022-10-31 18:17:27 +0100

    Added custom theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,23 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        reverseit: {
+          "primary": "#204a87",
+          "secondary": "#D926A9",
+          "accent": "#1FB2A6",
+          "neutral": "#191D24",
+          "base-100": "#2A303C",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272"
+        }
+      }
+    ],
+  },
   plugins: [
     require("@tailwindcss/typography"),
     require('daisyui')
