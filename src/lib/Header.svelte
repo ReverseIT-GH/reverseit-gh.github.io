6a241adad81f1fd34446c9b749820da7e4ac8d68
Luca D'Amico <damico.luca91@live.it>
2022-12-08 15:42:02 +0100

    Fixed random logo logic

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
     import { base } from '$app/paths';
+    import { onMount } from "svelte";
     let availableLogos: string[] = [
         'cfrevolution.png',
         'monsters.png',
         'wholecar.png'
     ];
 
-    function getRandomLogo() {
+    const randomLogo = getRandomLogo();
+
+    async function getRandomLogo() {
+        console.log('getRandomLogo called!');
         return availableLogos[Math.floor(Math.random() * availableLogos.length)];
     }
 </script>
 
 <div class="navbar mb-4 bg-base-200">
     <div class="flex-1">
-        <a href="{base}/"><img src="header_logo/{getRandomLogo()}" alt="ReverseIT"></a>
+        {#await randomLogo then logo}
+            <a href="{base}/"><img src="header_logo/{logo}" alt="ReverseIT"></a>
+        {/await}
     </div>
     <div class="flex-none">
         <ul class="menu menu-horizontal p-0">
