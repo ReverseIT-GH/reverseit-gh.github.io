1828559a804b8bc89d747cbf2d7f69c242787d03
Luca D'Amico <damico.luca91@live.it>
2022-12-08 22:17:27 +0100

    Fixed Latest Library Contents Ordering

--- src/lib/library-widget/LibraryWidget.svelte
+++ src/lib/library-widget/LibraryWidget.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
     import libraryIndex from "$lib/data/library/library.index.json";
-    import type { LibraryItem } from "src/types/LibraryItem";
+	import type { LibraryItem } from "src/types/LibraryItem";
     export let maxItemsNo = 0;
-    let libraryItems: LibraryItem[] = libraryIndex.slice(-maxItemsNo).reverse();
+
+    const libraryItems: LibraryItem[] = libraryIndex.slice(-maxItemsNo).sort((a,b)=> Date.parse(b.publication_date) - Date.parse(a.publication_date));
 </script>
 
 <ul class="text-sm list-disc list-outside text-left px-8">
     {#each libraryItems as libraryItem}
-    <li class="my-1">
-        <a class="tooltip inline"
-           data-tip="{new Date(Date.parse(libraryItem.publication_date)).toLocaleDateString()}"
-           href="{libraryItem.download_url}"
-           target="_blank"
-           rel="noreferrer">{@html libraryItem.title}</a>
-    </li>
+        <li class="my-1">
+            <a class="tooltip inline"
+            data-tip="{new Date(Date.parse(libraryItem.publication_date)).toLocaleDateString()}"
+            href="{libraryItem.download_url}"
+            target="_blank"
+            rel="noreferrer">{@html libraryItem.title}</a>
+        </li>
     {/each}
 </ul>
